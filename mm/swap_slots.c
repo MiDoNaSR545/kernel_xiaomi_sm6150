8441e41c5c60fbcbe5f28be9a832c5ff53317662
Huang Ying <ying.huang@intel.com>
2018-08-21 21:52:20 -0700

    mm, swap, get_swap_pages: use entry_size instead of cluster in parameter
    As suggested by Matthew Wilcox, it is better to use "int entry_size"
    instead of "bool cluster" as parameter to specify whether to operate for
    huge or normal swap entries.  Because this improve the flexibility to
    support other swap entry size.  And Dave Hansen thinks that this
    improves code readability too.
    
    So in this patch, the "bool cluster" parameter of get_swap_pages() is
    replaced by "int entry_size".
    
    And nr_swap_entries() trick is used to reduce the binary size when
    !CONFIG_TRANSPARENT_HUGE_PAGE.
    
           text	   data	    bss	    dec	    hex	filename
    base  24215	   2028	    340	  26583	   67d7	mm/swapfile.o
    head  24123	   2004	    340	  26467	   6763	mm/swapfile.o
    
    Link: http://lkml.kernel.org/r/20180720071845.17920-7-ying.huang@intel.com
    Signed-off-by: "Huang, Ying" <ying.huang@intel.com>
    Suggested-by: Matthew Wilcox <willy@infradead.org>
    Acked-by: Dave Hansen <dave.hansen@linux.intel.com>
    Cc: Daniel Jordan <daniel.m.jordan@oracle.com>
    Cc: Michal Hocko <mhocko@suse.com>
    Cc: Johannes Weiner <hannes@cmpxchg.org>
    Cc: Shaohua Li <shli@kernel.org>
    Cc: Hugh Dickins <hughd@google.com>
    Cc: Minchan Kim <minchan@kernel.org>
    Cc: Rik van Riel <riel@redhat.com>
    Cc: Dan Williams <dan.j.williams@intel.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- mm/swap_slots.c
+++ mm/swap_slots.c
@@ -264,8 +264,8 @@ static int refill_swap_slots_cache(struct swap_slots_cache *cache)
 
 	cache->cur = 0;
 	if (swap_slot_cache_active)
-		cache->nr = get_swap_pages(SWAP_SLOTS_CACHE_SIZE, false,
-					   cache->slots);
+		cache->nr = get_swap_pages(SWAP_SLOTS_CACHE_SIZE,
+					   cache->slots, 1);
 
 	return cache->nr;
 }
@@ -314,7 +314,7 @@ swp_entry_t get_swap_page(struct page *page)
 
 	if (PageTransHuge(page)) {
 		if (IS_ENABLED(CONFIG_THP_SWAP))
-			get_swap_pages(1, true, &entry);
+			get_swap_pages(1, &entry, HPAGE_PMD_NR);
 		return entry;
 	}
 
@@ -353,8 +353,7 @@ repeat:
 			return entry;
 	}
 
-	get_swap_pages(1, false, &entry);
-
+	get_swap_pages(1, &entry, 1);
 	return entry;
 }
 
