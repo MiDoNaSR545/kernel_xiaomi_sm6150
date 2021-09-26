57d12799b5185d495d16b243fe4d127303ad248f
Sultan Alsawaf <sultan@kerneltoast.com>
2021-09-27 00:22:37 +0530

    proc: cmdline: Patch SafetyNet flags
    Userspace parses androidboot.* flags from /proc/cmdline and sets the
    ro.boot.* props accordingly, which in turn trips SafetyNet when the
    reported values are incorrect.
    
    Patch the cmdline flags checked by SafetyNet to prevent it from failing
    a device. These flags were found by extracting the latest snet.jar and
    searching for 'ro.boot.' strings.
    
    Signed-off-by: Sultan Alsawaf <sultan@kerneltoast.com>
    Signed-off-by: Danda420 <dandagaharu77@gmail.com>

--- fs/proc/cmdline.c
+++ fs/proc/cmdline.c
@@ -39,8 +39,31 @@ static int cmdline_proc_show(struct seq_file *m, void *v)
 		return 0;
 	}
 #endif
-	seq_printf(m, "%s\n", saved_command_line);
+	seq_printf(m, "%s\n", new_command_line);
 	return 0;
+}
+
+static void patch_flag(char *cmd, const char *flag, const char *val)
+{
+	size_t flag_len, val_len;
+	char *start, *end;
+
+	start = strstr(cmd, flag);
+	if (!start)
+		return;
+
+	flag_len = strlen(flag);
+	val_len = strlen(val);
+	end = start + flag_len + strcspn(start + flag_len, " ");
+	memmove(start + flag_len + val_len, end, strlen(end) + 1);
+	memcpy(start + flag_len, val, val_len);
+}
+
+static void patch_safetynet_flags(char *cmd)
+{
+	patch_flag(cmd, "androidboot.flash.locked=", "1");
+	patch_flag(cmd, "androidboot.verifiedbootstate=", "green");
+	patch_flag(cmd, "androidboot.veritymode=", "enforcing");
 }
 
 static int cmdline_proc_open(struct inode *inode, struct file *file)
@@ -57,6 +80,13 @@ static const struct file_operations cmdline_proc_fops = {
 
 static int __init proc_cmdline_init(void)
 {
+	strcpy(new_command_line, saved_command_line);
+
+	/*
+	 * Patch various flags from command line seen by userspace in order to
+	 * pass SafetyNet checks.
+	 */
+	patch_safetynet_flags(new_command_line);
 #ifdef CONFIG_INITRAMFS_IGNORE_SKIP_FLAG
 	proc_command_line_init();
 #endif
