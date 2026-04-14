67e00369acbf4f54b5245ab5e3574a5009292c6f
Danda420 <dandagaharu77@gmail.com>
2026-04-14 12:43:34 +0700

    proc: cmdline: patch device_state cmdline aswell
    Signed-off-by: Danda420 <dandagaharu77@gmail.com>

--- fs/proc/cmdline.c
+++ fs/proc/cmdline.c
@@ -1,20 +1,25 @@
 // SPDX-License-Identifier: GPL-2.0
+
 #include <linux/fs.h>
 #include <linux/init.h>
 #include <linux/proc_fs.h>
 #include <linux/seq_file.h>
+#include <linux/string.h>
+
 #ifdef CONFIG_INITRAMFS_IGNORE_SKIP_FLAG
 #include <asm/setup.h>
 #endif
 
-#ifdef CONFIG_INITRAMFS_IGNORE_SKIP_FLAG
-#define INITRAMFS_STR_FIND "skip_initramf"
-#define INITRAMFS_STR_REPLACE "want_initramf"
-#define INITRAMFS_STR_LEN (sizeof(INITRAMFS_STR_FIND) - 1)
-
 static char proc_command_line[COMMAND_LINE_SIZE];
+static char new_command_line[COMMAND_LINE_SIZE];
 
-static void proc_command_line_init(void) {
+#ifdef CONFIG_INITRAMFS_IGNORE_SKIP_FLAG
+#define INITRAMFS_STR_FIND    "skip_initramf"
+#define INITRAMFS_STR_REPLACE "want_initramf"
+#define INITRAMFS_STR_LEN     (sizeof(INITRAMFS_STR_FIND) - 1)
+
+static void proc_command_line_init(void)
+{
 	char *offset_addr;
 
 	strcpy(proc_command_line, saved_command_line);
@@ -23,7 +28,9 @@ static void proc_command_line_init(void) {
 	if (!offset_addr)
 		return;
 
-	memcpy(offset_addr, INITRAMFS_STR_REPLACE, INITRAMFS_STR_LEN);
+	memcpy(offset_addr,
+	       INITRAMFS_STR_REPLACE,
+	       INITRAMFS_STR_LEN);
 }
 #endif
 
@@ -39,11 +46,14 @@ static int cmdline_proc_show(struct seq_file *m, void *v)
 		return 0;
 	}
 #endif
+
 	seq_printf(m, "%s\n", new_command_line);
 	return 0;
 }
 
-static void patch_flag(char *cmd, const char *flag, const char *val)
+static void patch_flag(char *cmd,
+		       const char *flag,
+		       const char *val)
 {
 	size_t flag_len, val_len;
 	char *start, *end;
@@ -54,21 +64,42 @@ static void patch_flag(char *cmd, const char *flag, const char *val)
 
 	flag_len = strlen(flag);
 	val_len = strlen(val);
-	end = start + flag_len + strcspn(start + flag_len, " ");
-	memmove(start + flag_len + val_len, end, strlen(end) + 1);
+
+	end = start + flag_len +
+	      strcspn(start + flag_len, " ");
+
+	memmove(start + flag_len + val_len,
+		end,
+		strlen(end) + 1);
+
 	memcpy(start + flag_len, val, val_len);
 }
 
 static void patch_safetynet_flags(char *cmd)
 {
-	patch_flag(cmd, "androidboot.flash.locked=", "1");
-	patch_flag(cmd, "androidboot.verifiedbootstate=", "green");
-	patch_flag(cmd, "androidboot.veritymode=", "enforcing");
+	patch_flag(cmd,
+		   "androidboot.flash.locked=",
+		   "1");
+
+	patch_flag(cmd,
+		   "androidboot.vbmeta.device_state=",
+		   "locked");
+
+	patch_flag(cmd,
+		   "androidboot.verifiedbootstate=",
+		   "green");
+
+	patch_flag(cmd,
+		   "androidboot.veritymode=",
+		   "enforcing");
 }
 
-static int cmdline_proc_open(struct inode *inode, struct file *file)
+static int cmdline_proc_open(struct inode *inode,
+			     struct file *file)
 {
-	return single_open(file, cmdline_proc_show, NULL);
+	return single_open(file,
+			   cmdline_proc_show,
+			   NULL);
 }
 
 static const struct file_operations cmdline_proc_fops = {
@@ -83,15 +114,21 @@ static int __init proc_cmdline_init(void)
 	strcpy(new_command_line, saved_command_line);
 
 	/*
-	 * Patch various flags from command line seen by userspace in order to
-	 * pass SafetyNet checks.
+	 * Patch userspace-visible cmdline flags
+	 * for SafetyNet / Play Integrity spoofing
 	 */
 	patch_safetynet_flags(new_command_line);
+
 #ifdef CONFIG_INITRAMFS_IGNORE_SKIP_FLAG
 	proc_command_line_init();
 #endif
 
-	proc_create("cmdline", 0, NULL, &cmdline_proc_fops);
+	proc_create("cmdline",
+		    0,
+		    NULL,
+		    &cmdline_proc_fops);
+
 	return 0;
 }
+
 fs_initcall(proc_cmdline_init);
